8436d947901223911484c70a3e10615b8d327cde
Shariful_Islam <sharifulislam.shawon.916@gmail.com>
2025-01-28 16:29:40 +0600

    Update shariful.java

--- shariful.java
+++ shariful.java
@@ -1,6 +1,6 @@
 public class shariful{
     public static void main(String[] args){
-        System.out.println("Heelo This is Shariful");
+        System.out.println("Hello This is Shariful");
     }
 
 }
